1cc9dc2fa403598910458dd057c1fc3fac6d0d08
Jean-Paul Khawam <44164731+Jayy77@users.noreply.github.com>
2021-05-29 14:27:37 +0300

    removed unnecessary functions

--- Problem 4 - Binary Search Tree.cpp
+++ Problem 4 - Binary Search Tree.cpp
@@ -91,13 +91,7 @@ public:
    
     //--------------------------------------------------------
     // THIS IS JUST THE CLASS TREE 
-    //constructor
     Node(int data) {
-        m_data = data;
-    }
-
-    //in case you need to change data of a node (since it is private)
-    void setData(int data) {
         m_data = data;
     }
 
@@ -109,20 +103,6 @@ public:
         else return this;
     }
 
-    //returns pointer to the minimum element
-    Node* findMinimum() {
-        if (this == nullptr) return nullptr;
-        else if (m_left == nullptr) return this;
-        else return m_left->findMinimum();
-    }
-
-    //returns pointer to the maximum element
-    Node* findMaximum() {
-        if (this == nullptr) return nullptr;
-        else if (m_right == nullptr) return this;
-        else return m_right->findMaximum();
-    }
-
     Node* insert(int data) {
         if (this == nullptr) {
             Node* newNode = new Node(data);
@@ -132,28 +112,6 @@ public:
         else m_right = m_right->insert(data);
         return this;
     }
-
-    void displayInOrder() {
-        if (this == nullptr) return;
-        m_left->displayInOrder();
-        std::cout << m_data << " ";
-        m_right->displayInOrder();
-    }
-
-    void displayPostOrder() {
-        if (this == nullptr) return;
-        m_left->displayPostOrder();
-        m_right->displayPostOrder();
-        std::cout << m_data << " ";
-    }
-
-    void displayPreOrder() {
-        if (this == nullptr) return;
-        std::cout << m_data << " ";
-        m_left->displayPreOrder();
-        m_right->displayPreOrder();
-    }
-
 };
 
 
