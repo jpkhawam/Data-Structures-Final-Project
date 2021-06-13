d22d8480e80b4b34b2598d9defb2aabf82612e74
Jean-Paul Khawam <44164731+Jayy77@users.noreply.github.com>
2021-06-13 15:27:35 +0300

    Better formatting and different output range

--- Problem 2 - Priority Queues.cpp
+++ Problem 2 - Priority Queues.cpp
@@ -36,7 +36,7 @@ public:
         Node* current = this->front;
         int size = 1;
         while (true) {
-            if(current==this->rear) break;
+            if (current == this->rear) break;
             current = current->next;
             size++;
         }
@@ -73,7 +73,7 @@ public:
 std::vector<int> inputData(std::vector<Queue>& queues, int roundNumber) {
 
     // Initialize our mersenne twister with a random seed based on the clock
-    std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)*roundNumber) };
+    std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr) * roundNumber) };
     // Create a reusable random number generator that generates uniform numbers between 1 and 10
     //we can cap it at 10, can be anything else
     std::uniform_int_distribution<> rng{ 1, 10 };
@@ -99,8 +99,8 @@ int outputData(std::vector<Queue>& queues) {
     // Initialize our mersenne twister with a random seed based on the clock
     std::mt19937 mersenne{ static_cast<std::mt19937::result_type>(std::time(nullptr)) };
     // Create a reusable random number generator that generates
-    // uniform numbers between total_size/2 and total_size
-    std::uniform_int_distribution<> rng{ total_size / 2, total_size };
+    // uniform numbers between total_size/4 and total_size
+    std::uniform_int_distribution<> rng{ total_size / 3, total_size };
 
     int amount_to_output = rng(mersenne);
 
@@ -118,22 +118,26 @@ int outputData(std::vector<Queue>& queues) {
 int main()
 {
     std::vector<Queue> queues(number_of_queues);
-
+    std::cout << "Number of queues: " << number_of_queues << std::endl;
     int amount_of_rounds = 0;
-    std::cout << "How many rounds would you like to run? ";
+    std::cout << "How many rounds would you like to run?" << std::endl;
     std::cin >> amount_of_rounds;
     std::cout << std::endl << std::endl;
 
-    for(int round = 0; round < amount_of_rounds; round++){
-        std::vector<int> random_numbers = inputData(queues, round);
-        int amount_outputted = outputData(queues);
+    for (int round = 1; round <= amount_of_rounds; round++) {
+        std::cout << "Round " << round << ": " << std::endl;
 
+        std::vector<int> random_numbers = inputData(queues, round);
         std::cout << "Numbers inputted into each queue: ";
         for (const int& i : random_numbers)
             std::cout << i << "  ";
+        std::cout << "\nSize of queues after input: " << std::endl;
+        for (int i = 0; i < queues.size(); i++)
+            std::cout << "q" << i << ".size(): " << queues.at(i).size() << '\n';
 
-        std::cout << "\nAmount of data outputted from the system: " << amount_outputted << '\n';\
-
+        int amount_outputted = outputData(queues);
+        std::cout << "\nAmount of data outputted from the system: " << amount_outputted << '\n'; \
+            std::cout << "Size of queues after output: " << std::endl;
         for (int i = 0; i < queues.size(); i++)
             std::cout << "q" << i << ".size(): " << queues.at(i).size() << '\n';
         std::cout << std::endl << std::endl;
